04750cc8f72eaaf9189c5ebd7fa443380af40fb1
Emiel Harmsen <emielch@gmail.com>
2015-02-24 13:58:46 +0100

    first version example
    works with the OctoWS2811 library for the teensy 3.0/3.1

---
+++ examples/colore_example/colore_example.ino
@@ -0,0 +1,54 @@
+#include <Colore.h>
+#include <OctoWS2811.h>
+
+#define PIN 6
+#define LEDS 60
+#define BEAMS 10
+Beam beams[BEAMS];
+
+const int ledsPerStrip = LEDS;
+DMAMEM int displayMemory[ledsPerStrip*6];
+int drawingMemory[ledsPerStrip*6];
+const int config = WS2811_GRB | WS2811_800kHz;
+OctoWS2811 leds(ledsPerStrip, displayMemory, drawingMemory, config);
+
+uint16_t leds1[] = {0,1,2,3,4};
+uint16_t leds2[] = {5,6,7,8,9,10,11,12,13,14,15,16,17};
+
+Segment seg1( sizeof(leds1)/2, leds1 );
+Segment seg2( sizeof(leds2)/2, leds2 );
+
+Colore colore;
+
+Segment *segArray[] = {
+  &seg1,
+  &seg2
+};
+
+void setup(){
+  leds.begin();
+  colore.begin(LEDS, segArray, sizeof(segArray)/4, beams, BEAMS, &set_ledLib, &get_ledLib, &show_ledLib );
+}
+
+
+void loop(){
+  colore.update();
+}
+
+
+void set_ledLib(int pixel, byte r, byte g, byte b){
+  leds.setPixel(pixel,r,g,b);
+}
+
+void show_ledLib(){
+  leds.show();
+}
+
+Color get_ledLib(int pixel){
+  uint32_t conn = leds.getPixel(pixel);  // retrieve the color that has already been saved
+  byte b = conn & 255;       // unpack the color
+  byte g = conn >> 8 & 255;
+  byte r = conn >> 16 & 255;
+  Color pixelCol(r,g,b,RGB_MODE);
+  return pixelCol;
+}
